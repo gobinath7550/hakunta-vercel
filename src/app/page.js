6245b0b6a79015059fd34e9c78a18fee5b23e036
gobinath7550 <gobinath7550@gmail.com>
2025-01-05 22:16:24 +0530

    Removed unwanted footer links

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import Image from "next/image";
 import AOS from "aos";
 import {
--- src/components/global/Footer.js
+++ src/components/global/Footer.js
@@ -15,7 +15,7 @@ export default function Footer() {
         <footer className="bg-[#233888]">
             <div className="max-w-7xl m-auto p-6 py-12">
                 <div className="xl:grid xl:grid-cols-3 xl:gap-8">
-                    <div className="space-y-4">
+                    <div className="space-y-8">
                         <div>
                             <a
                                 href="/"
@@ -40,7 +40,7 @@ export default function Footer() {
                                 target="_blank"
                                 className="text-gray-200 hover:text-gray-200 border-[1px] border-[#7e7e7e] hover:border-white transition-all p-2 rounded-lg"
                             >
-                                <span className="sr-only">Linkedin</span>
+                                <span className="sr-only">{"Linkedin"}</span>
                                 <LinkedIn className="fill-white h-6 w-6" />
                             </a>
                             <a
@@ -48,7 +48,7 @@ export default function Footer() {
                                 target="_blank"
                                 className="text-gray-200 hover:text-gray-200 border-[1px] border-[#7e7e7e] hover:border-white transition-all p-2 rounded-lg"
                             >
-                                <span className="sr-only">X</span>
+                                <span className="sr-only">{"X"}</span>
                                 <XTwitter className="fill-white h-6 w-6" />
                             </a>
                             <a
@@ -56,7 +56,7 @@ export default function Footer() {
                                 target="_blank"
                                 className="text-gray-200 hover:text-gray-200 border-[1px] border-[#7e7e7e] hover:border-white transition-all p-2 rounded-lg"
                             >
-                                <span className="sr-only">Youtube</span>
+                                <span className="sr-only">{"Youtube"}</span>
                                 <Youtube className="fill-white h-6 w-6" />
                             </a>
                             <a
@@ -64,7 +64,7 @@ export default function Footer() {
                                 target="_blank"
                                 className="text-gray-200 hover:text-gray-200 border-[1px] border-[#7e7e7e] hover:border-white transition-all p-2 rounded-lg"
                             >
-                                <span className="sr-only">Facebook</span>
+                                <span className="sr-only">{"Facebook"}</span>
                                 <Facebook className="fill-white h-6 w-6" />
                             </a>
                             <a
@@ -72,7 +72,7 @@ export default function Footer() {
                                 target="_blank"
                                 className="text-gray-200 hover:text-gray-200 border-[1px] border-[#7e7e7e] hover:border-white transition-all p-2 rounded-lg"
                             >
-                                <span className="sr-only">Instagram</span>
+                                <span className="sr-only">{"Instagram"}</span>
                                 <Instagram className="fill-white h-6 w-6" />
                             </a>
                             <a
@@ -80,44 +80,77 @@ export default function Footer() {
                                 target="_blank"
                                 className="text-gray-200 hover:text-gray-200 border-[1px] border-[#7e7e7e] hover:border-white transition-all p-2 rounded-lg"
                             >
-                                <span className="sr-only">Email</span>
+                                <span className="sr-only">{"Email"}</span>
                                 <Email className="fill-none stroke-white h-6 w-6" />
                             </a>
                         </div>
                     </div>
-                    <div className="mt-16 grid grid-cols-2 gap-8 xl:col-span-2 xl:mt-0">
-                        <div className="md:grid md:grid-cols-2 md:gap-8">
+                    <div className="mt-10 grid md:grid-cols-2 md:gap-8 xl:col-span-2 xl:mt-0">
+                        <div className="grid grid-cols-2 gap-8">
                             <div>
                                 <h3 className="text-md font-semibold leading-6 text-white">
-                                    {"Our Solutions"}
+                                    {"Company"}
                                 </h3>
                                 <ul role="list" className="mt-6 space-y-4">
                                     <li>
                                         <a
-                                            href="/aiplatform"
+                                            href="/aboutus"
                                             className="text-md leading-6 text-gray-300 hover:text-gray-50"
                                         >
-                                            {"AI Platform"}
+                                            {"About Us"}
                                         </a>
                                     </li>
                                     <li>
                                         <a
-                                            href="/aialgorithms"
+                                            href="/careers"
                                             className="text-md leading-6 text-gray-300 hover:text-gray-50"
                                         >
-                                            {"AI Algorithms"}
+                                            {"Careers"}
                                         </a>
                                     </li>
                                     <li>
                                         <a
-                                            href="/industryapplications"
+                                            href="/contactus"
                                             className="text-md leading-6 text-gray-300 hover:text-gray-50"
                                         >
-                                            {"Industry Applications"}
+                                            {"Contact Us"}
                                         </a>
                                     </li>
                                 </ul>
                             </div>
+                            <div>
+                                <h3 className="text-md font-semibold leading-6 text-white">
+                                    {"Resources"}
+                                </h3>
+                                <ul role="list" className="mt-6 space-y-4">
+                                    <li>
+                                        <a
+                                            href="/blogs"
+                                            className="text-md leading-6 text-gray-300 hover:text-gray-50"
+                                        >
+                                            {"Blogs"}
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a
+                                            href="/terms"
+                                            className="text-md leading-6 text-gray-300 hover:text-gray-50"
+                                        >
+                                            {"Terms of Service"}
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a
+                                            href="/privacy"
+                                            className="text-md leading-6 text-gray-300 hover:text-gray-50"
+                                        >
+                                            {"Privacy Policy"}
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="grid grid-cols-1 gap-8">
                             <div className="mt-10 md:mt-0">
                                 <h3 className="text-md font-semibold leading-6 text-white">
                                     {"Use Cases"}
@@ -150,93 +183,13 @@ export default function Footer() {
                                 </ul>
                             </div>
                         </div>
-                        <div className="md:grid md:grid-cols-2 md:gap-8">
-                            <div>
-                                <h3 className="text-md font-semibold leading-6 text-white">
-                                    {"Resources"}
-                                </h3>
-                                <ul role="list" className="mt-6 space-y-4">
-                                    <li>
-                                        <a
-                                            href="/pricing"
-                                            className="text-md leading-6 text-gray-300 hover:text-gray-50"
-                                        >
-                                            {"Pricing"}
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a
-                                            href="/blog"
-                                            className="text-md leading-6 text-gray-300 hover:text-gray-50"
-                                        >
-                                            {"Blog"}
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a
-                                            href="/casestudies"
-                                            className="text-md leading-6 text-gray-300 hover:text-gray-50"
-                                        >
-                                            {"Case Studies"}
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a
-                                            href="/terms"
-                                            className="text-md leading-6 text-gray-300 hover:text-gray-50"
-                                        >
-                                            {"Terms of Service"}
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a
-                                            href="/privacy"
-                                            className="text-md leading-6 text-gray-300 hover:text-gray-50"
-                                        >
-                                            {"Privacy Policy"}
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div className="mt-10 md:mt-0">
-                                <h3 className="text-md font-semibold leading-6 text-white">
-                                    {"Company"}
-                                </h3>
-                                <ul role="list" className="mt-6 space-y-4">
-                                    <li>
-                                        <a
-                                            href="/aboutus"
-                                            className="text-md leading-6 text-gray-300 hover:text-gray-50"
-                                        >
-                                            {"About Us"}
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a
-                                            href="/careers"
-                                            className="text-md leading-6 text-gray-300 hover:text-gray-50"
-                                        >
-                                            {"Careers"}
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a
-                                            href="/contactus"
-                                            className="text-md leading-6 text-gray-300 hover:text-gray-50"
-                                        >
-                                            {"Contact Us"}
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
                     </div>
                 </div>
                 <div className="mt-16 border-t border-gray-400/30 pt-8 sm:mt-20 lg:mt-24">
                     <div className="text-md text-center text-white">
                         {"Copyright © 2024"}
                         <a rel="noopener" href="/">
-                            {" Hakunta Info Tech Private Limited"}
+                            {" Hakunta Consultancy Services Pvt. Ltd."}
                         </a>
                     </div>
                 </div>
